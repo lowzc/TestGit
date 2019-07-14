6aada42b1b61cc740a872a50c50f94dd6ce27b17
hzc <857412548@qq.com>
2019-07-14 18:39:54 +0800

    初始化

---
+++ src/main/java/TestGit.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *@author tom
+ *@date  2019/7/14 18:34
+ *@email   huangzucheng888@126com
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("test...");
+    }
+}
